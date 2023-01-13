3e00092ff0b4c1243b6e316e4074f9668f981b63
jcav99 <106625114+jcav99@users.noreply.github.com>
2023-01-13 13:56:50 -0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 app.get('/', (req,res)=>{
     res.send('Hello there');
 });
@@ -19,6 +20,22 @@ app.get('/api/courses/:id', (req,res)=> {
     }
         res.send(course);
 })
+//HTTP POST requests
+app.post('api/courses', (req,res) => {
+    // you write the if code here
+    //add an if statement so that the name of the course you post is .min(3) characters 
+    const course ={
+        //we assign an ID and a name property
+        id: course.length +1,
+        name:req.body.name
+    }
+        //YOU WRITE THE NEXT LINES OF code
+      //next step: push it to the array
+        //next step: the server should return the new resource to the client in the body of the response
+    
+      
+});
+    
 app.listen(3000, () => {
     console.log("Listneing on port 3000 ...")
 })
