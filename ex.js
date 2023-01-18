bb10c737690a6e80f588e23f87d67597e77e27ee
jcav99 <106625114+jcav99@users.noreply.github.com>
2023-01-18 13:39:09 -0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -21,13 +21,21 @@ app.get('/api/courses/:id', (req,res)=> {
         res.send(course);
 })
 //HTTP POST requests
-app.post('api/courses', (req,res) => {
+app.post('/api/courses', (req,res) => {
     // you write the if code here
     //add an if statement so that the name of the course you post is .min(3) characters 
-    const course ={
-        //we assign an ID and a name property
-        id: course.length +1,
-        name:req.body.name
+    if (req.body.name.length >= 3){
+        const course ={
+            //we assign an ID and a name property
+            id: courses.length +1,
+            name:req.body.name
+        }
+        courses.push(course);
+        res.send(course);
+    }
+    else{
+        res.status(404).send("Error. Course name too short");
+        return
     }
         //YOU WRITE THE NEXT LINES OF code
       //next step: push it to the array
@@ -35,6 +43,28 @@ app.post('api/courses', (req,res) => {
     
       
 });
+//here we need the specific id of the course we want to update
+app.put('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if (!course){
+        res.status(404).send("The course with the gived ID was not found");
+        return
+    }
+    course.name = req.body.name;
+    res.send(course);
+});
+app.delete('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if (!course){
+        res.status(404).send("The course with the gived ID was not found");
+        return
+    }
+    let ind = courses.indexOf(req.params.id);
+    courses.splice(ind,1);
+    res.send("Course was successfully deleted.");
+});
+
+    
     
 app.listen(3000, () => {
     console.log("Listneing on port 3000 ...")
